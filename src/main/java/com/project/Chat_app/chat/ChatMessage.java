b9b16bf4c076a262fa88ae6505435c9b3eac408b
Karthikeya Namuduri <karthikeyanamuduri@Karthikeyas-Laptop.local>
2024-08-29 17:56:50 +0530

    Initial commit

---
+++ src/main/java/com/project/Chat_app/chat/ChatMessage.java
@@ -0,0 +1,17 @@
+package com.project.Chat_app.chat;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class ChatMessage {
+    private String content;
+    private String sender;
+    private MessageType type;
+
+
+}
---
+++ src/main/java/com/project/Chat_app/chat/MessageType.java
@@ -0,0 +1,8 @@
+package com.project.Chat_app.chat;
+
+public enum MessageType {
+
+    CHAT,
+    JOIN,
+    LEAVE
+}
